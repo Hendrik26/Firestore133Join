8b4c9c7d50b4099afcf834aa843d41e4719a3599
Hendrik26 <hendrik.herrdler@gmail.com>
2019-02-14 17:51:43 +0100

    Connection to Database works.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {AngularFireAuthModule} from 'angularfire2/auth';
 
-const config = {
+const configOld = {
   apiKey: 'AIzaSyBogpc-AUVgVdcgoiWhE4_lKCFzfcbaSaA',
   authDomain: 'angularfirebase-267db.firebaseapp.com',
   databaseURL: 'https://angularfirebase-267db.firebaseio.com',
@@ -16,6 +16,16 @@ const config = {
   storageBucket: 'angularfirebase-267db.appspot.com',
   messagingSenderId: '96751198234'
 };
+
+const config = {
+    apiKey: 'AIzaSyCK4yXTgsJltFSecpLSXpYYcsb5FL8Ltag',
+    authDomain: 'firestore133joins-12828.firebaseapp.com',
+    databaseURL: 'https://firestore133joins-12828.firebaseio.com',
+    projectId: 'firestore133joins-12828',
+    storageBucket: 'firestore133joins-12828.appspot.com',
+    messagingSenderId: '322337869305'
+};
+
 
 @NgModule({
   declarations: [AppComponent],
